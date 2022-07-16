2900fa7a093c5a3399d76a5efbd79fa9ca872a8d
VincentChris <vincentchris@126.com>
2022-07-16 08:18:47 +0800

    添加飞书配置文件和http模块

--- apps/gateway/src/environments/environment.ts
+++ apps/gateway/src/environments/environment.ts
@@ -1,3 +1,9 @@
 export const environment = {
   production: false,
+  FEISHU_CONFIG: {
+    FEISHU_URL: 'https://open.feishu.cn/open-apis',
+    FEISHU_API_HOST: 'https://open.feishu.cn',
+    FEISHU_APP_ID: 'cli_a24010a4b138d013',
+    FEISHU_APP_SECRET: 'U1hhN1orP7VBLv1fTXVCicp5BdCkL0g6',
+  },
 };
