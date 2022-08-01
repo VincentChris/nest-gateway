df53533af002bd16c91d8b9a53503c1a3622175f
VincentChris <vincentchris@126.com>
2022-08-01 10:35:44 +0800

    全量添加element

--- apps/admin/src/views/Home.vue
+++ apps/admin/src/views/Home.vue
@@ -1,11 +1,12 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png" />
+    <el-button>Default</el-button>
     <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
   </div>
 </template>
 
-<script>
+<script lang="ts">
 import { defineComponent } from 'vue';
 import HelloWorld from '../components/HelloWorld.vue';
 
