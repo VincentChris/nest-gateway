e0bdffcd5181e206add4da4eed5c53e3885186b5
VincentChris <vincentchris@126.com>
2022-08-13 10:03:32 +0800

    feature:vue 添加element自动引入

--- apps/admin/configure-webpack.js
+++ apps/admin/configure-webpack.js
@@ -1,3 +1,6 @@
+const AutoImport = require('unplugin-auto-import/webpack');
+const Components = require('unplugin-vue-components/webpack');
+const { ElementPlusResolver } = require('unplugin-vue-components/resolvers');
 /*
  * Modify the webpack config by exporting an Object or Function.
  *
@@ -10,4 +13,14 @@
  *
  * https://cli.vuejs.org/config/#configurewebpack
  */
-module.exports = (config) => {};
+module.exports = {
+  // ...
+  plugins: [
+    AutoImport({
+      resolvers: [ElementPlusResolver()],
+    }),
+    Components({
+      resolvers: [ElementPlusResolver()],
+    }),
+  ],
+};
